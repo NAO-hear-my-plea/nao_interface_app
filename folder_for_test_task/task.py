8d9caedf59575b72d1d75d277872d8e2e84af0ed
xatxx <a.talabskagmail.com>
2024-04-15 21:03:28 +0200

    your message

--- folder_for_test_task/task.py
+++ folder_for_test_task/task.py
@@ -1,8 +1,19 @@
 import unittest
 
-def uniq_sum(first: list, second: list) -> list:
-    # write your code here
-    pass
+
+def uniq_sum(list1, list2) -> list:
+    unique = []
+    list = list1 + list2
+    for el in list:
+        if list.count(el) <= 1:
+            unique.append(el)
+        else:
+            if el not in unique:
+                unique.append(el)
+
+    unique.sort()
+
+    return unique
 
 class TestUniqSum(unittest.TestCase):
     def test_uniq_sum(self):
@@ -14,3 +25,7 @@ class TestUniqSum(unittest.TestCase):
         self.assertEqual([1, 2, 3], uniq_sum([2, 1, 2], [3, 1, 3]))
         self.assertEqual([1, 2, 3], uniq_sum([1, 3, 1, 3, 1, 3, 1, 3], [2, 2, 2, 2, 2, 3, 3, 2, 3]))
         self.assertEqual([1, 2, 4, 5, 9], uniq_sum([1, 2, 4], [4, 5, 2, 9, 9, 9, 5, 1, 4]))
+
+
+if __name__ == '__main__':
+    unittest.main()
